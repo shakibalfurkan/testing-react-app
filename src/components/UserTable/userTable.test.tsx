ad7d633dd49f27b19052568e392278694609e226
shakibalfurkan <shakibalfurkan@gmail.com>
2025-09-17 10:22:37 +0600

    within method and debugging explored

--- src/components/UserTable/userTable.test.tsx
+++ src/components/UserTable/userTable.test.tsx
@@ -1,16 +1,26 @@
-import { findByTestId, render, screen } from "@testing-library/react";
+import {
+  findByTestId,
+  logRoles,
+  render,
+  screen,
+  within,
+} from "@testing-library/react";
 import UserTable from "./UserTable";
 import { users } from "../../App";
 
 describe("UserTable()", () => {
-  it("should render the welcome message after 5ms", async () => {
-    render(<UserTable users={users} />);
-    const welcomeMessageEl = await screen.findByTestId(
-      "welcome",
-      {},
-      { timeout: 2000 }
-    );
+  it("should render the table correctly", async () => {
+    const { container } = render(<UserTable users={users} />);
+    logRoles(container);
+    // const welcomeMessageEl = await screen.findByTestId(
+    //   "welcome",
+    //   {},
+    //   { timeout: 2000 }
+    // );
     // screen.logTestingPlaygroundURL();
-    expect(welcomeMessageEl).toBeInTheDocument();
+    // expect(welcomeMessageEl).toBeInTheDocument();
+
+    const rows = within(screen.getByTestId("users")).getAllByRole("row");
+    expect(rows).toHaveLength(4);
   });
 });
